eb3d0937c69c07b4431528909047c0bee6097fd6
Shrimp <lcamara0916@gmail.com>
2022-08-03 19:21:11 +0000

    Fixed the possible moves in one of the borders

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -2,20 +2,29 @@ let gameSquares = document.querySelectorAll('.board-square:not(.white)');
 let redPieces = document.querySelectorAll('.red-piece');
 let whitePieces = document.querySelectorAll('.white-piece')
 
-console.log(redPieces);
+/*console.log(redPieces);
 console.log(whitePieces);
 console.log(gameSquares);
+*/
 
-for (let i = 0; i < redPieces.length; i++){
+for (let i = 0; i < redPieces.length; i++) {
     redPieces[i].addEventListener('click', () => {
         console.log(i);
-        for(let i = 0; i < gameSquares.length; i++){
-            if(gameSquares[i].classList.contains('selected')){
+
+        //this removes all the current blue squares
+        for (let i = 0; i < gameSquares.length; i++) {
+            if (gameSquares[i].classList.contains('selected')) {
                 gameSquares[i].classList.remove('selected');
             }
         }
-        console.log(gameSquares[i]);
-        gameSquares[i + 3].classList.add('selected');
-        gameSquares[i + 4].classList.add('selected');
+
+        //this adds the blue squares
+
+        if (i % 8 === 0) {
+            gameSquares[i + 4].classList.add('selected');
+        } else {
+            gameSquares[i + 3].classList.add('selected');
+            gameSquares[i + 4].classList.add('selected');
+        }
     })
 }
